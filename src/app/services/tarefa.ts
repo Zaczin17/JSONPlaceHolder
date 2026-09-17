63b11e4b8994fc2ea288d00b9d62b979090380a7
Nathan Pedreira Ferrreira <Napedfer@gmail.com>
2026-09-17 09:35:54 -0300

    feat: implement task management form with reactive forms
    - Added a form for task creation with fields for title, description, priority, category, status, and due date.
    - Integrated reactive forms for form handling and validation.
    - Created a service to handle task creation via HTTP POST to a placeholder API.
    - Updated the task model to include necessary fields.
    - Implemented error handling and task list display after creation.

--- src/app/services/tarefa.ts
+++ src/app/services/tarefa.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@angular/core';
-
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Tarefa } from '../models/tarefa';
 @Injectable({
   providedIn: 'root',
 })
-export class Tarefa {}
+export class TarefaService {
+  private http = inject(HttpClient);
+  private api = 'https://jsonplaceholder.typicode.com/posts';
+  criar(tarefa: Tarefa) {
+    return this.http.post<Tarefa>(this.api, tarefa);
+  }
+}
